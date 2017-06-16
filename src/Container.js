639162ab77ce836e322caf1934b0af4e5105a433
Jeffrey Bowden <jbowden@opinionlab.com>
2017-06-16 08:41:33 -0500

    Added close card button

--- src/Container.js
+++ src/Container.js
@@ -141,6 +141,16 @@ class Container extends React.Component {
     });
   }
 
+  closeCard = (index) => {
+    const cards = _cloneDeep(this.state.editCards);
+
+    cards.splice(index, 1);
+
+    this.setState({
+      editCards: cards
+    });
+  }
+
   render () {
     return (
       <div className='container-wrapper'>
@@ -159,6 +169,7 @@ class Container extends React.Component {
               <QuestionLibraryList addQuestion={this.addQuestion} questionLibrary={questionLibrary} />
               <div className='container-right-column'>
                 <Edit
+                  closeCard={this.closeCard}
                   editCards={this.state.editCards}
                   isRequiredChange={this.isRequiredChange}
                   moveCard={this.moveCard} />
